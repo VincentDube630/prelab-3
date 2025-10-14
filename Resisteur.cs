12b0ed3ce753c4c23baf8a5e45bdffbb120a9224
vince <6304790@cstj.qc.ca>
2025-10-14 15:54:51 -0400

    Ajoutez des fichiers projet.

---
+++ Resisteur.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prelab_3
+{
+    internal class Resisteur
+    {
+        public double Ohm {  get; set; }
+        public double tension { get; set; }
+        public Resisteur Resisteur1 { get; set; }
+        public Resisteur Resisteur2 { get; set; }
+        public string Type { get; set; }
+
+        public Resisteur(double omh)
+        {
+            Ohm = omh;
+        }
+        public Resisteur(Resisteur resisteur1,Resisteur resisteur2,string type)
+        {
+            Resisteur1 = resisteur1;
+            Resisteur2 = resisteur2;
+            Type = type;
+            double valeur1 = resisteur1.Ohm;
+            double valeur2 = resisteur2.Ohm;
+            if (type == "p")
+            {
+                Ohm =  1 / (1 / Convert.ToDouble(valeur1) + 1 / Convert.ToDouble(valeur2));
+            }
+            else if (type == "s")
+            {
+                Ohm =  Convert.ToDouble(valeur1) + Convert.ToDouble(valeur2);
+
+            }
+            else
+            {
+                Ohm = 0;
+            }
+        }
+
+        public double Ohm1(Tension)
+        {
+            double valeur1 = Tension.tension;
+
+            return Tension / Ohm;
+        }
+
+    }
+}
---
+++ Tension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prelab_3
+{
+    internal class Tension
+    {
+        public double Volt {  get; set; }
+         
+        public Tension(double volt)
+        {
+            Volt = volt;
+        }
+    }
+}
